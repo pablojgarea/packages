6d90a23a38ce864150ef8ae4239eb6dfcb28590c
pablojgarea <pablojgarea@gmail.com>
2014-10-27 14:04:20 +0100

    Update carga_calendario.php

--- subasta/blocks/calendario_bootstrap/tools/carga_calendario.php
+++ subasta/blocks/calendario_bootstrap/tools/carga_calendario.php
@@ -1,23 +1,30 @@
-<?php 
-defined('C5_EXECUTE') or die("Access Denied.");
 
-Loader::model('subasta','subasta');
+<?php  defined('C5_EXECUTE') or die("Access Denied.");?>
+<?php
 
-$db = Loader::db();
-
+$db    = Loader::db();
 $start = $_REQUEST['from'] / 1000;
 $end   = $_REQUEST['to'] / 1000;
-$sql   = sprintf('SELECT * FROM events WHERE `datetime` BETWEEN %s and %s',
-    $db->quote(date('Y-m-d', $start)), $db->quote(date('Y-m-d', $end)));
 
 
-$subastas = array();
-echo sizeof($lista_subastas);
-foreach ($lista_subastas as $subasta):
-	$subastas[]=$subasta->getCalendarJSON();
-	echo var_dump($subasta->getCalendarJSON());
-endforeach;
+$query   = sprintf('SELECT * FROM subasta WHERE `fecha` BETWEEN %s and %s',$db->quote(date('Y-m-d', $start)), $db->quote(date('Y-m-d', $end)));
 
-echo json_encode(array('success' => 1, 'result' => $subastas));
+$subastas = $db->getAll($query);
+
+
+$out = array();
+foreach($subastas as $row) {
+    $out[] = array(
+        'id' => $row['id'],
+        'title' => $row['tipo_bienes'].'::'.$row['localizacion'].'::'.$row['fecha'],
+        'url' => 'http://www.google.es',
+        'class'=> 'event-important',
+        'start' => strtotime($row['fecha']) . '000',
+        'end' => strtotime($row['fecha']) .'000'
+    );
+}
+
+echo json_encode(array('success' => 1, 'result' => $out));
 exit;
+
 ?>
