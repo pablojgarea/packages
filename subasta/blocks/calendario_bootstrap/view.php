65bc2f754ffc45496961b4e62f16d799e6cd7b37
pablojgarea <pablojgarea@gmail.com>
2014-10-27 14:01:36 +0100

    Update view.php

--- subasta/blocks/calendario_bootstrap/view.php
+++ subasta/blocks/calendario_bootstrap/view.php
@@ -31,53 +31,80 @@ endforeach;
     <div id="calendar"></div>
 </div>
     <script type="text/javascript">
-	var options = {
+        $(document).ready(function (){
+        var options = {
         tmpl_path: "/webc5/packages/subasta/blocks/calendario_bootstrap/tmpls/",
 //        events_source: function () {  return  [<?php echo json_encode(array('success' => 1, 'result' => $subastas)); ?>];},
-		events_source: '/webc5/packages/subasta/blocks/calendario_bootstrap/events.json.php',
-//		events_source: '/webc5/packages/subasta/blocks/calendario_bootstrap/tools/carga_calendario.php',
+//              events_source: '/blocks/calendario_bootstrap/events.json.php',
+                events_source: '<?php echo $tool_name ?>',
 
-		view: 'month',
-		tmpl_path: "/webc5/packages/subasta/blocks/calendario_bootstrap/tmpls/",
-		tmpl_cache: false,
-		day: '2013-03-12',
-		onAfterEventsLoad: function(events) {
-			if(!events) {
-				return;
-			}
-			var list = $('#eventlist');
-			list.html('');
+                view: 'month',
+                tmpl_path: "/blocks/calendario_bootstrap/tmpls/",
+                tmpl_cache: false,
+                day: '2014-10-24',
+                onAfterEventsLoad: function(events) {
+                        if(!events) {
+                                return;
+                        }
+                        var list = $('#eventlist');
+                        list.html('');
 
-			$.each(events, function(key, val) {
-				$(document.createElement('li'))
-					.html('<a href="' + val.url + '">' + val.title + '</a>')
-					.appendTo(list);
-			});
-		},
-		onAfterViewLoad: function(view) {
-			$('.page-header h3').text(this.getTitle());
-			$('.btn-group button').removeClass('active');
-			$('button[data-calendar-view="' + view + '"]').addClass('active');
-		},
-		classes: {
-			months: {
-				general: 'label'
-			}
-		}
-	};
+                        $.each(events, function(key, val) {
+                                $(document.createElement('li'))
+                                        .html('<a href="' + val.url + '">' + val.title + '</a>')
+                                        .appendTo(list);
+                        });
+                },
+                onAfterViewLoad: function(view) {
+                        $('.page-header h3').text(this.getTitle());
+                        $('.btn-group button').removeClass('active');
+                        $('button[data-calendar-view="' + view + '"]').addClass('active');
+                },
+                classes: {
+                        months: {
+                                general: 'label'
+                        }
+                }
+        };
 
-	var calendar = $('#calendar').calendar(options);
+        var calendar = $('#calendar').calendar(options);
+
+        $('.btn-group button[data-calendar-nav]').each(function() {
+                var $this = $(this);
+                $this.click(function() {
+                        calendar.navigate($this.data('calendar-nav'));
+                });
+        });
+
+        $('.btn-group button[data-calendar-view]').each(function() {
+                var $this = $(this);
+                $this.click(function() {
+                        calendar.view($this.data('calendar-view'));
+                });
+        });
+
+        $('#first_day').change(function(){
+                var value = $(this).val();
+                value = value.length ? parseInt(value) : null;
+                calendar.setOptions({first_day: value});
+                calendar.view();
+        });
+
+        $('#language').change(function(){
+                calendar.setLanguage($(this).val());
+                calendar.view();
+        });
+
+        $('#events-in-modal').change(function(){
+                var val = $(this).is(':checked') ? $(this).val() : null;
+                calendar.setOptions({modal: val});
+        });
+        $('#events-modal .modal-header, #events-modal .modal-footer').click(function(e){
+                //e.preventDefault();
+                //e.stopPropagation();
+        });
 
 
-		// $(document).ready(function(){
-  //       var calendar = $("#calendar").calendar(
-  //           {
-  //               language: 'es-ES',
-  //               day: '2013-03-12',
-  //               tmpl_path: "/webc5/packages/subasta/blocks/calendario_bootstrap/tmpls/",
-  //               events_source: function () {  return  [<?php echo json_encode(array('success' => 1, 'result' => $subastas)); ?>];},
-  //               events_url:''
-  //           });         	
-  //   });
+
+        });
     </script>
-
